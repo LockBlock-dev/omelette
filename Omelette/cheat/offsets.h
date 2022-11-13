a753876de63ad59e71f14b4e8fef84c6daa5bba8
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2022-11-13 16:42:37 +0100

    more offsets

--- Omelette/cheat/offsets.h
+++ Omelette/cheat/offsets.h
@@ -5,8 +5,17 @@
 
 namespace offsets
 {
-	extern uintptr_t moduleBase = (uintptr_t)GetModuleHandle(NULL);
+	inline uintptr_t moduleBase = (uintptr_t)GetModuleHandle(NULL);
 
-	extern uintptr_t lasersLevelPtr = moduleBase + 0x0005B1D0;
-	std::vector<unsigned int> lasersLevelOffsets = { 0x298, 0x38, 0xC4  };
+	// Player red/right/2
+	inline uintptr_t playerData = moduleBase + 0x0005B1D0;
+
+	inline std::vector<unsigned int> xCoordsOffsets = { 0x298, 0x38, 0x94 };
+	inline std::vector<unsigned int> yCoordsOffsets = { 0x298, 0x38, 0x98 };
+	inline std::vector<unsigned int> horizontalVelocity = { 0x298, 0x38, 0x9C };
+	inline std::vector<unsigned int> invincibilityTimeoutOffsets = { 0x298, 0x38, 0xBC };
+	inline std::vector<unsigned int> healthOffsets = { 0x298, 0x38, 0xC0 };
+	inline std::vector<unsigned int> lasersLevelOffsets = { 0x298, 0x38, 0xC4 };
+	inline std::vector<unsigned int> rocketsCountOffsets = { 0x298, 0x38, 0xC8 };
+	inline std::vector<unsigned int> scoreOffsets = { 0x298, 0x38, 0xD4 };
 }
