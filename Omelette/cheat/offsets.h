74207d5aa10f084f3f68a16a2e158e8f981a5ea2
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2022-11-13 15:08:34 +0100

    inline -> extern

--- Omelette/cheat/offsets.h
+++ Omelette/cheat/offsets.h
@@ -5,9 +5,8 @@
 
 namespace offsets
 {
-	inline uintptr_t moduleBase = (uintptr_t)GetModuleHandle(NULL);
+	extern uintptr_t moduleBase = (uintptr_t)GetModuleHandle(NULL);
 
-	inline uintptr_t lasersLevelAddr = 0x0005B1D0;
-	inline uintptr_t lasersLevelPtr = moduleBase + lasersLevelAddr;
-	std::vector<unsigned int> lasersLevelOffset = { 0x298, 0x38, 0xC4  };
+	extern uintptr_t lasersLevelPtr = moduleBase + 0x0005B1D0;
+	std::vector<unsigned int> lasersLevelOffsets = { 0x298, 0x38, 0xC4  };
 }
