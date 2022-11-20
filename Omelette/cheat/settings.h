654b7a805f6576f536cf5202e47e406e505c6ec3
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2022-11-20 16:29:02 +0100

    playerId state

--- Omelette/cheat/settings.h
+++ Omelette/cheat/settings.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <dinput.h> // Keyboard mapping from DirectInput
 
 
 namespace settings {
 	inline struct Cheats
 	{
 		bool showMenu = false;
+		int playerId = 0;
 
 		bool autoShoot = false;
 		int autoShootKey = DIKEYBOARD_RSHIFT;
