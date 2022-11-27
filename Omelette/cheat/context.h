e49c175baf600c0096112c3c6b8cde4b40759c86
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2022-11-27 14:54:58 +0100

    signed

--- Omelette/cheat/context.h
+++ Omelette/cheat/context.h
@@ -4,7 +4,6 @@
 
 
 #pragma pack(push, 1)
-struct GameInfo;
 struct PlayerData {
 	char _unk1[12]; // unknown
 	int _unk2;
@@ -12,18 +11,18 @@ struct PlayerData {
 	int x;
 	int y;
 	int xVelocity; // -10 (left), 0 (idle), 10 (right)
-	unsigned int playerId; // Blue = 0, Red = 1
-	unsigned int spaceshipAngle; // 0 (left), 4 (idle), 8 (right)
+	int playerId; // Blue = 0, Red = 1
+	int spaceshipAngle; // 0 (left), 4 (idle), 8 (right)
 	char _unk4[12];
 	int shieldPulseEffect; // -1 = close wide bubble, 1 = thin distant bubble
-	unsigned int invisibilityTimeout;
-	unsigned int shieldTimeout;
-	unsigned int lives;
-	unsigned int lasersLevel; // 0 to 7
-	unsigned int rocketsCount;
-	unsigned int consumedChickenLegs;
-	unsigned int scoreDisplay; // grows slowly
-	unsigned int score; // instant
+	int invisibilityTimeout;
+	int shieldTimeout;
+	int lives;
+	int lasersLevel; // 0 to 7
+	int rocketsCount;
+	int consumedChickenLegs;
+	int scoreDisplay; // grows slowly
+	int score; // instant
 };
 #pragma pack(pop)
 
@@ -31,7 +30,6 @@ namespace context {
 	inline uintptr_t moduleBase = (uintptr_t)GetModuleHandle(NULL);
 	inline unsigned int baseOffset = 0x5B1D0;
 
-	inline GameInfo* gameInfo = nullptr;
 	inline std::vector<unsigned int> inGameCheats = { 0x298, 0x64 };
 	inline PlayerData* playerData = nullptr;
 }
