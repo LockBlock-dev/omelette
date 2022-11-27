9885fce175188c95da4da1650f9ceaa698babe59
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2022-11-27 16:44:08 +0100

    imgui submodule

--- Omelette/cheat/gui.cpp
+++ Omelette/cheat/gui.cpp
@@ -1,6 +1,6 @@
-#include "../imgui/imgui.h"
-#include "../imgui/imgui_impl_dx9.h"
-#include "../imgui/imgui_impl_win32.h"
+#include "imgui.h"
+#include "backends/imgui_impl_dx9.h"
+#include "backends/imgui_impl_win32.h"
 
 #include "gui.h"
 #include "settings.h"
