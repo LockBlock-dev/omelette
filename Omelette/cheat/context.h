c3ed8498721ebc106f106da069d7dc68402cd96a
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2023-01-28 17:19:23 +0100

    Update context.h

--- Omelette/cheat/context.h
+++ Omelette/cheat/context.h
@@ -5,25 +5,49 @@
 
 #pragma pack(push, 1)
 struct PlayerData {
-	char _unk1[12]; // unknown
-	int _unk2;
-	char _unk3[132];
+	char _unk1[36]; // unknown
+	double size; // spaceship size default to 1
+	int _unk3;
+	int axis; // 0 = default, 2 mirrored vertical, 4 = mirrored horizontal
+	char _unk4[96];
 	int x;
 	int y;
 	int xVelocity; // -10 (left), 0 (idle), 10 (right)
 	int playerId; // Blue = 0, Red = 1
 	int spaceshipAngle; // 0 (left), 4 (idle), 8 (right)
-	char _unk4[12];
+	int _unk5;
+	int _unk6;
+	int _unk7;
 	int shieldPulseEffect; // -1 = close wide bubble, 1 = thin distant bubble
 	int invisibilityTimeout;
 	int shieldTimeout;
 	int lives;
-	int lasersLevel; // 0 to 7
 	int firepower; // 0 to 7
 	int rocketsCount;
 	int consumedChickenLegs;
 	int scoreDisplay; // grows slowly
 	int score; // instant
+};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+struct ChickenData {
+	int x;
+	int y;
+	char _unk1[12];
+	double size; // chicken size default to 1
+	int _unk2;
+	int axis; // 0 = default, 2 mirrored vertical, 4 = mirrored horizontal
+	char _unk3[92];
+	int health;
+	int spawnTimeout;
+	int xBase;
+	int yBase;
+	int x_2;
+	int y_2;
+	char _unk4[12];
+	int movements; // 0 = idle, 1 = moving
+
 };
 #pragma pack(pop)
 
